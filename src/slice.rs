89a88d6c15e3f229528e2cb72f2e75e0a3231993
Vitaliy Solodilov <mcdoker18@gmail.com>
2025-11-02 16:01:45 +0300

    find peak element
    https://leetcode.com/problems/find-peak-element/

--- src/slice.rs
+++ src/slice.rs
@@ -81,3 +81,90 @@ mod max_product_test {
         }
     }
 }
+
+// https://leetcode.com/problems/find-peak-element/
+pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+    assert!(!nums.is_empty());
+
+    if nums.len() == 1 {
+        return 0;
+    }
+
+    let (mut i, mut j) = (0, nums.len() - 1);
+
+    while i <= j {
+        let mid_index = (j - i) / 2 + i;
+        let mid_value = nums[mid_index];
+        let after_mid_value = if mid_index != nums.len() - 1 {
+            Some(nums[mid_index + 1])
+        } else {
+            None
+        };
+        let before_mid_value = if mid_index != 0 {
+            Some(nums[mid_index - 1])
+        } else {
+            None
+        };
+        println!("i={i} j={j} mid_index={mid_index} vec={:?}", &nums);
+
+        if after_mid_value.is_none_or(|v| mid_value > v)
+            && before_mid_value.is_none_or(|v| mid_value > v)
+        {
+            return mid_index as i32;
+        }
+
+        if after_mid_value.is_some_and(|v| v > mid_value) {
+            i = mid_index + 1;
+            continue;
+        }
+
+        if before_mid_value.is_some_and(|v| v > mid_value) {
+            j = mid_index;
+            continue;
+        }
+    }
+
+    unreachable!()
+}
+
+#[cfg(test)]
+mod find_peak_element_test {
+    use super::*;
+
+    #[test]
+    fn all() {
+        struct TestCase {
+            nums: Vec<i32>,
+            expected: i32,
+        }
+
+        let tests = [
+            TestCase {
+                nums: vec![1],
+                expected: 0,
+            },
+            TestCase {
+                nums: vec![1, 2],
+                expected: 1,
+            },
+            TestCase {
+                nums: vec![2, 1],
+                expected: 0,
+            },
+            TestCase {
+                nums: vec![1, 2, 3, 1],
+                expected: 2,
+            },
+            TestCase {
+                nums: vec![1, 2, 1, 3, 5, 6, 4],
+                expected: 5,
+            },
+        ];
+
+        for tc in tests {
+            let desc = format!("vec={:?}", tc.nums);
+
+            assert_eq!(tc.expected, find_peak_element(tc.nums), "{desc}");
+        }
+    }
+}
