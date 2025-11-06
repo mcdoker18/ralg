254d26896eed11228371d60138e636c385a67b3c
Vitaliy Solodilov <mcdoker18@gmail.com>
2025-11-06 20:48:54 +0300

    replace with greatest
    https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/

--- src/number.rs
+++ src/number.rs
@@ -28,3 +28,44 @@ mod maximum69_number_tests {
         }
     }
 }
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+pub fn replace_elements_with_greatest(mut arr: Vec<i32>) -> Vec<i32> {
+    assert!(!arr.is_empty());
+
+    let mut curr_max = *arr.last().unwrap();
+    *(arr.last_mut().unwrap()) = -1;
+
+    for elem in arr.iter_mut().rev().skip(1) {
+        let elem_value = *elem;
+        *elem = curr_max;
+
+        if elem_value > curr_max {
+            curr_max = elem_value;
+        }
+    }
+
+    arr
+}
+
+#[cfg(test)]
+mod replace_elements_with_greatest_tests {
+    use super::*;
+
+    #[test]
+    fn all() {
+        let tests = [
+            (vec![1], vec![-1]),
+            (vec![2], vec![-1]),
+            (vec![3, 4], vec![4, -1]),
+            (vec![5, 4], vec![4, -1]),
+            (vec![17, 18, 5, 4, 6, 1], vec![18, 6, 6, 6, 1, -1]),
+            (vec![5, 5, 5, 3, 2, 4], vec![5, 5, 4, 4, 4, -1]),
+        ];
+
+        for tc in tests {
+            let desc = format!("{:?}", &tc.0);
+            assert_eq!(tc.1, replace_elements_with_greatest(tc.0), "{desc}");
+        }
+    }
+}
